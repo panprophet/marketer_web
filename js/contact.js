12dabe8142477aba2acdec8d8879c9dc49532c3c
ycas2002 <ycas2002@yahoo.com>
2019-01-31 00:38:55 +0100

    proba efekta

--- js/contact.js
+++ js/contact.js
@@ -1,10 +1,29 @@
 function nextStep(number) {
   var move = -1 * number;
+  var effDiv = (number/100) + 1;
   var count = document.getElementById("contactwrap").children.length;
-console.log(number, move);
 
   for(var i = 1; i <= count; i++) {
+
+    if((effDiv - 1) === i) {
+
+      document.getElementById("strana" + i).classList.remove("fade_in_cont");
+
+      document.getElementById("strana" + i).classList.add("fade_out_cont");
+
+    }
+
     document.getElementById("strana" + i).style.transform = "translateX(" + move + "%)";
+
+    if(effDiv === i) {
+
+      document.getElementById("strana" + i).classList.remove("fade_out_cont");
+
+      document.getElementById("strana" + i).classList.add("fade_in_cont");
+
+    }
+
+    // document.getElementById("strana" + i).style.transform = "translateX(" + move + "%)";
   }
 
 }
