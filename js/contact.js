5ab75236dca2794687e4682bac832c830ff9357b
ycas2002 <ycas2002@yahoo.com>
2019-02-08 12:42:56 +0100

    responsive7

--- js/contact.js
+++ js/contact.js
@@ -16,5 +16,48 @@ function nextStep(number) {
     }
 
   }
-
 }
+
+var inputArray = document.querySelectorAll("input");
+
+inputArray.forEach(function(elem){
+  elem.addEventListener("keyup", function(){
+    var elem1;
+    var elem2;
+    if(elem1 !== '') {
+      elem1 = elem.id;
+    } else if (elem1 !== elem.id) {
+      elem2.elem.id;
+    }
+    console.log(elem.id, elem1, elem2);
+
+    // if( elem.value !== '' && elem.getElementById("nazivkompanije") !== '') {
+    //   document.getElementById("next1").classList.remove('next-disabled');
+    //   document.getElementById("next1").classList.add('next-enabled');
+    // } else {
+    //   document.getElementById("next1").classList.remove('next-enabled');
+    //   document.getElementById("next1").classList.add('next-disabled')
+    // }
+    // if( elem.getElementById("webpage") !== '' && elem.getElementById("cimesebavimo") !== '') {
+    //   document.getElementById("next2").classList.remove('next-disabled');
+    //   document.getElementById("next2").classList.add('next-enabled');
+    // } else {
+    //   document.getElementById("next2").classList.remove('next-enabled');
+    //   document.getElementById("next2").classList.add('next-disabled')
+    // }
+    // if( elem.getElementById("interesovanje") !== '') {
+    //   document.getElementById("next3").classList.remove('next-disabled');
+    //   document.getElementById("next3").classList.add('next-enabled');
+    // } else {
+    //   document.getElementById("next3").classList.remove('next-enabled');
+    //   document.getElementById("next3").classList.add('next-disabled')
+    // }
+    // if( elem.getElementById("email") !== '' && elem.getElementById("telefon") !== '') {
+    //   document.getElementById("next4").classList.remove('next-disabled');
+    //   document.getElementById("next4").classList.add('next-enabled');
+    // } else {
+    //   document.getElementById("next4").classList.remove('next-enabled');
+    //   document.getElementById("next4").classList.add('next-disabled')
+    // }
+  });
+});
