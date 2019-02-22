e74e9be79aa2231a3d496476fef501f28d7198ce
ycas2002 <ycas2002@yahoo.com>
2019-02-22 16:59:14 +0100

    kontakt final

--- js/contact.js
+++ js/contact.js
@@ -1,31 +1,41 @@
-// var re = /^(([^<>()\[\]\.,;:\s@\"]+(\.[^<>()\[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
+var re = /^(([^<>()\[\]\.,;:\s@\"]+(\.[^<>()\[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
 
 var inputArray = document.querySelectorAll("input");
     var elem1;
     var elem2;
     var pagecount = 1;
+    var prevmove = 0;
+
 function nextStep(number) {
   var move = -1 * number;
   var effDiv = (number/100) + 1;
   var count = document.getElementById("contactwrap").children.length;
 
   for(var i = 1; i <= count; i++) {
-
     if(document.getElementById("strana" + i).classList.contains("fade_in_cont")) {
       document.getElementById("strana" + i).classList.remove("fade_in_cont");
       document.getElementById("strana" + i).classList.add("fade_out_cont");
     }
-
     if(effDiv === i) {
       document.getElementById("strana" + i).classList.remove("fade_out_cont");
       document.getElementById("strana" + i).classList.add("fade_in_cont");
-      pagecount = pagecount + 1;
+      if(prevmove >= move) {
+        prevmove = move;
+        pagecount = pagecount + 1;
+      } else {
+        prevmove = move;
+        pagecount = pagecount - 1;
+      }
       elem1 = null;
       elem2 = null;
     }
   }
 }
-
+document.addEventListener("keydown", function(e){
+  if(e.which == 9){
+    e.preventDefault();
+  }
+});
 inputArray.forEach(function(elem) {
   elem.addEventListener("keyup", function(){
     if(!elem1) {
@@ -33,12 +43,21 @@ inputArray.forEach(function(elem) {
     } else if (elem1.id !== elem.id) {
       elem2 = elem;
     }
+    if(elem.id === 'email' ) {
+      elem.addEventListener("focusout", ()=> {
+        if(!re.test(elem.value)) {
+          document.getElementById("mailerror").classList.remove("email-hidden");
+          document.getElementById("mailerror").classList.add("email-shown");
+          elem.focus();
+        } else {
+          document.getElementById("mailerror").classList.remove("email-shown");
+          document.getElementById("mailerror").classList.add("email-hidden");
+        }
+      });
+    }
 
     if(elem1 && elem2) {
       if(elem1.value !== '' && elem2.value !== '') {
-        // if(elem1.id === 'email') {
-
-        // }
         document.getElementById("next" + pagecount).classList.remove('next-disabled');
         document.getElementById("next" + pagecount).classList.add('next-enabled');
       } else {
